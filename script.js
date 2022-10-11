2c1fd0ed6024749a07f079b04fe225dd96112001
manuvai <rm90266@gmail.com>
2022-10-11 21:48:22 +0200

    feat: Ajout de l'exo 2.8

--- script.js
+++ script.js
@@ -5,6 +5,8 @@
 function testDisponibilite(id) {
     let nbVoyageurs = parseInt(document.getElementById(id).value);
     let isValid = true;
+    let destinationChoice = document.getElementById("destination-choice").value;
+    let paymentByVacationCheck = document.getElementById("vacation-check").checked;
 
     if (isNaN(nbVoyageurs)) {
         isValid = false;
@@ -15,6 +17,12 @@ function testDisponibilite(id) {
     } else if (nbVoyageurs > 20) {
         isValid = false;
         alert("Veuillez renseigner un nombre inférieur à 20 personnes");
+    } else if (destinationChoice == 'foix' && nbVoyageurs < 5) {
+        isValid = false;
+        alert("Le voyage à Foix n’est disponible que pour 5 personnes ou plus");
+    } else if (paymentByVacationCheck && (nbVoyageurs != 1 && nbVoyageurs != 2)) {
+        isValid = false;
+        alert("Le mode de paiement « Chèques vacances » n’est possible que pour 1 ou 2 personnes");
     }
 
     return isValid;
