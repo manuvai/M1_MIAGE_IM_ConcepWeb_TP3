1e08c5fe406b9adb0d79db09037acbb6fee9dd2a
manuvai <rm90266@gmail.com>
2022-10-11 21:09:50 +0200

    feat: Ajout de l'exo 2.3.a

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 /**
  * @author manuvai.rehua@ut-capitole.fr
  */
+
+function testDisponibilite(id) {
+    let element = parseInt(document.getElementById(id).value);
+    if (isNaN(element)) {
+        return alert("Veuillez renseigner un vrai nombre");
+    }
+    if (element < 0) {
+        return alert("Veuillez renseigner un nombre positif");
+    }
+}
